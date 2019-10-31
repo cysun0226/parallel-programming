be7c27e60b8224334fe6bec1f52843b1fbf382d7
Chia-Yu, Sun <chiayusun.cs08g@nctu.edu.tw>
2019-11-01 01:39:34 +0800

    add xorshift

--- hw1/pi.cpp
+++ hw1/pi.cpp
@@ -5,11 +5,22 @@
 #include "pthread.h"
 #include <math.h>
 #include <iostream>
+#include <chrono>
+#include <unistd.h>
 
 long thread_count;
 unsigned long long total_number_in_circle;
 unsigned long long thread_toss_num;
 pthread_mutex_t mutex;
+
+// random function (xorshift)
+
+uint64_t xorshift64(uint64_t xorshift64_state){
+	xorshift64_state ^= xorshift64_state << 13;
+	xorshift64_state ^= xorshift64_state >> 7;
+	xorshift64_state ^= xorshift64_state << 17;
+	return xorshift64_state;
+}
 
 void* Thread_toss(void* tn){
     unsigned long long number_in_circle = 0, toss;
@@ -18,12 +29,23 @@ void* Thread_toss(void* tn){
     std::default_random_engine generator(rd());
     std::uniform_real_distribution<float> unif(-1.0, 1.0);
     
+    uint64_t state_1 = generator();
+    uint64_t state_2 = generator();
+    
+    
     for (toss = 0; toss < thread_toss_num; toss++) {        
         
         // x, y = random double between -1 and 1;
         double x, y, distance_squared;
-        x = unif(generator);
-        y = unif(generator);
+
+        state_1 = xorshift64(state_1);
+        state_2 = xorshift64(state_2);
+         
+        x = ((double)state_1 / (double)(UINT64_MAX >> 1)) -1;
+        y = ((double)state_2 / (double)(UINT64_MAX >> 1)) -1;
+
+        // x = unif(generator);
+        // y = unif(generator);
         
         distance_squared = x*x + y*y;
         if (distance_squared <= 1)
@@ -49,6 +71,9 @@ int main(int argc, char **argv)
         exit(-1);
     }
 
+    // Record start time
+    auto start = std::chrono::high_resolution_clock::now();
+
     
     // prepare for thread creation
     long       thread;  // Use long in case of a 64-bit system 
@@ -73,5 +98,11 @@ int main(int argc, char **argv)
     double pi_estimate = 4*total_number_in_circle/((double) number_of_tosses);
     
     printf("%f\n",pi_estimate);
+    
+    // Record end time
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "exec time = " << elapsed.count() << " s" << std::endl;
+
     return 0;
 }
