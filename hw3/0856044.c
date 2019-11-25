98ab49c260c5f125b37c5162a967271dd8257ab4
Chia-Yu, Sun <chiayusun.cs08g@nctu.edu.tw>
2019-11-26 01:31:40 +0800

    seperate the for loop, confirm the result remains the same

--- hw3/0856044.c
+++ hw3/0856044.c
@@ -1,13 +1,28 @@
 // #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#define CPU_NUM 4
+
 #ifndef W
 #define W 20                                    // Width
 #endif
+
+typedef struct {
+    int up;
+    int low;
+} Range;
+
 int main(int argc, char **argv) {
+  if (argc < 3){
+    printf("please input L, iter, seed\n");
+    exit(0);
+  }
+
   int L = atoi(argv[1]);                        // Length
   int iteration = atoi(argv[2]);                // Iteration
   srand(atoi(argv[3]));                         // Seed
+  
   float d = (float) random() / RAND_MAX * 0.2;  // Diffusivity
   int *temp = malloc(L*W*sizeof(int));          // Current temperature
   int *next = malloc(L*W*sizeof(int));          // Next time step
@@ -21,18 +36,35 @@ int main(int argc, char **argv) {
   // MPI_Comm_size(MPI_COMM_WORLD, &size);
   // printf("Hello world from rank %d out of %d processors\n", rank, size);
 
+  int interval = L / CPU_NUM;
+  Range range[CPU_NUM];
+  for (size_t i = 0; i < CPU_NUM; i++){
+    range[i].low = i*interval;
+    range[i].up = (i+1)*interval;
+  }
+
   // seperate this loop by i
+  /*
   for (int i = 0; i < L; i++) {
     for (int j = 0; j < W; j++) {
       temp[i*W+j] = random()>>3;
     }
   }
+  */
+ for (size_t r = 0; r < CPU_NUM; r++){
+   for (int i = range[r].low; i < range[r].up; i++) {
+    for (int j = 0; j < W; j++) {
+      temp[i*W+j] = random()>>3;
+    }
+  }
+ }
 
   
   int count = 0, balance = 0;
   while (iteration--) {     // Compute with up, left, right, down points
     balance = 1;
     count++;
+    /*
     for (int i = 0; i < L; i++) {
       for (int j = 0; j < W; j++) {
         float t = temp[i*W+j] / d; 
@@ -64,6 +96,43 @@ int main(int argc, char **argv) {
         }
       }
     }
+    */
+   for (size_t r = 0; r < CPU_NUM; r++)
+   {
+     for (int i = range[r].low; i < range[r].up; i++) {
+      for (int j = 0; j < W; j++) {
+        float t = temp[i*W+j] / d; 
+        // t = temp[i][j] / d
+
+        t += temp[i*W+j] * -4;   
+        // t += temp[i][j] * -4
+        
+        t += temp[(i - 1 <  0 ? 0 : i - 1) * W + j];
+        // t += tmp[i-1][j]
+
+        t += temp[(i + 1 >= L ? i : i + 1)*W+j];
+        // t += tmp[i+1][j]
+
+        t += temp[i*W+(j - 1 <  0 ? 0 : j - 1)];
+        // t += tmp[i][j-1]
+
+        t += temp[i*W+(j + 1 >= W ? j : j + 1)];
+        // t += tmp[i][j+1]
+
+        t *= d;
+        // t *= d
+
+        next[i*W+j] = t ;
+        // next[i][j] = t
+
+        if (next[i*W+j] != temp[i*W+j]) {
+          balance = 0;
+        }
+      }
+    }
+   }
+
+
     if (balance) {
       break;
     }
@@ -75,13 +144,19 @@ int main(int argc, char **argv) {
   int min = temp[0];
 
   // parallel ?
-  for (int i = 0; i < L; i++) {
+  for (size_t r = 0; r < CPU_NUM; r++)
+  {
+    for (int i = range[r].low; i < range[r].up; i++) {
     for (int j = 0; j < W; j++) {
       if (temp[i*W+j] < min) {
         min = temp[i*W+j];
       }
     }
   }
+  }
+  
+
+  
 
   // exit MPI
   // MPI_Finalize();
