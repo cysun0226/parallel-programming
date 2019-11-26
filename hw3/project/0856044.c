d9a02da0522e0f62b2b73553ed608f98a1d8596b
Chia-Yu, Sun <chiayusun.cs08g@nctu.edu.tw>
2019-11-27 01:33:36 +0800

    add mpi send/receive sample

--- hw3/project/0856044.c
+++ hw3/project/0856044.c
@@ -1,13 +1,13 @@
-// #include "mpi.h"
+#include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define CPU_NUM 4
-
 #ifndef W
 #define W 20                                    // Width
 #endif
+
+#define MAIN_RANK 0
 
 typedef struct {
     int up;
@@ -34,42 +34,32 @@ int main(int argc, char **argv) {
   // ----------------------------------------------------------------------------
   // MPI_Init(&argc,&argv);
   // ----------------------------------------------------------------------------
-  // MPI_Init(NULL, NULL);
-  // int rank, size;
-  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  // MPI_Comm_size(MPI_COMM_WORLD, &size);
-  // printf("Hello world from rank %d out of %d processors\n", rank, size);
+  MPI_Init(NULL, NULL);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  printf("Hello world from rank %d out of %d processors\n", rank, size);
 
-  int interval = L / CPU_NUM;
-  Range range[CPU_NUM];
-  for (size_t i = 0; i < CPU_NUM; i++){
+  int interval = L / size;
+  Range range[size];
+  for (size_t i = 0; i < size; i++){
     range[i].low = i*interval;
     range[i].up = (i+1)*interval;
   }
 
   // seperate this loop by i
-  /*
   for (int i = 0; i < L; i++) {
     for (int j = 0; j < W; j++) {
       temp[i*W+j] = random()>>3;
     }
   }
-  */
- for (size_t r = 0; r < CPU_NUM; r++){
-   for (int i = range[r].low; i < range[r].up; i++) {
-    for (int j = 0; j < W; j++) {
-      temp[i*W+j] = random()>>3;
-    }
-  }
- }
-
   
   int count = 0, balance = 0;
   while (iteration--) {     // Compute with up, left, right, down points
     balance = 1;
     count++;
-    /*
-    for (int i = 0; i < L; i++) {
+   for (int i = range[rank].low; i < range[rank].up; i++) {
+      //  printf("i=%d\n", i);
       for (int j = 0; j < W; j++) {
         float t = temp[i*W+j] / d; 
         // t = temp[i][j] / d
@@ -100,42 +90,6 @@ int main(int argc, char **argv) {
         }
       }
     }
-    */
-   int random_idx[] = {3, 1, 0, 2};
-   for (size_t r = 0; r < CPU_NUM; r++)
-   {
-     for (int i = range[random_idx[r]].low; i < range[random_idx[r]].up; i++) {
-      for (int j = 0; j < W; j++) {
-        float t = temp[i*W+j] / d; 
-        // t = temp[i][j] / d
-
-        t += temp[i*W+j] * -4;   
-        // t += temp[i][j] * -4
-        
-        t += temp[(i - 1 <  0 ? 0 : i - 1) * W + j];
-        // t += tmp[i-1][j]
-
-        t += temp[(i + 1 >= L ? i : i + 1)*W+j];
-        // t += tmp[i+1][j]
-
-        t += temp[i*W+(j - 1 <  0 ? 0 : j - 1)];
-        // t += tmp[i][j-1]
-
-        t += temp[i*W+(j + 1 >= W ? j : j + 1)];
-        // t += tmp[i][j+1]
-
-        t *= d;
-        // t *= d
-
-        next[i*W+j] = t ;
-        // next[i][j] = t
-
-        if (next[i*W+j] != temp[i*W+j]) {
-          balance = 0;
-        }
-      }
-    }
-   }
 
 
     if (balance) {
@@ -147,24 +101,34 @@ int main(int argc, char **argv) {
   }
 
   int min = temp[0];
+  int tag = 0;
 
-  // parallel ?
-  for (size_t r = 0; r < CPU_NUM; r++)
-  {
-    for (int i = range[r].low; i < range[r].up; i++) {
+  // find self min
+  for (int i = range[rank].low; i < range[rank].up; i++) {
     for (int j = 0; j < W; j++) {
       if (temp[i*W+j] < min) {
         min = temp[i*W+j];
       }
     }
   }
-  }
-  
 
-  clock_t end = clock();  
+  // send / collect data
+  MPI_Status status;
+  if (rank == 0){ // master
+    for (size_t i = 1; i < size; i++){
+      int part_min;
+      MPI_Recv(&part_min, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+      printf("receive: %d\n", part_min);
+    }
+  }
+  else{ // worker
+    MPI_Send(&min, 1, MPI_INT, MAIN_RANK, tag, MPI_COMM_WORLD);
+  }
 
   // exit MPI
-  // MPI_Finalize();
+  MPI_Finalize();
+
+  clock_t end = clock();  
 
   printf("Size: %d*%d, Iteration: %d, Min Temp: %d\n", L, W, count, min);
 
